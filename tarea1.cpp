767ebbcbc83a61cb799cb0fbaccf1cdfa684e043
ramon <ramon.rha@gmail.com>
2018-12-29 11:08:45 -0600

    Funciones maxSubSum completadas. Número opcional de iteraciones como parametro.

--- tarea1.cpp
+++ tarea1.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <string>
 
-int maxSubSum1(std::vector<int> &a )
+int maxSubSum1(const std::vector<int> &a )
 {
 	int maxSum = 0;
 	int size = (int)a.size()-2;
@@ -22,8 +23,7 @@ int maxSubSum1(std::vector<int> &a )
 	}
 	return maxSum ;
 }
-int
-maxSubSum2( const std::vector<int> &a ){
+int maxSubSum2( const std::vector<int> &a ){
 	int maxSum = 0;
 	int size = (int)a.size()-2;
 	for(int i = 0; i < size; i++ ){
@@ -56,15 +56,18 @@ int max3(int a, int b, int c){
 int MaxSumRec(const std::vector<int> &a,int left, int right){
 	if(left == right){  // Base case
 		if( a[left] > 0 ){
+			//std::cout<<"-";
 			return a[left];
 		}
 		else{
+			//std::cout<<"+";
 			return 0;
 		}
 	}
 	int center = (int)(( (float)left + (float)right ) / 2.0);
+	//std::cout<<"["<<left<<","<<center<<","<<right<<"]"<<std::endl;
 	int maxLeftSum = MaxSumRec( a, left, center );
-	int maxRightSum = MaxSumRec ( a, center + 1, right);
+	int maxRightSum = MaxSumRec ( a, center+1, right);
 	int maxLeftBorderSum = 0, leftBorderSum = 0;
 	for(int i = center; i >= left; i--){
 		leftBorderSum += a[i];
@@ -100,29 +103,39 @@ int maxSubSum4(const std::vector<int> &a){
 	return maxSum;
 }
 
-int main (){
+int main (int argc, char *argv[]){
 	std::default_random_engine generator;
 	std::uniform_int_distribution<int> distribution(std::numeric_limits<int>::min() ,
 			std::numeric_limits<int>::max());
 	//int dice_roll = distribution(generator);  // generates number in the range 1..6
 
+	int Max_Data_Size = 1000;
+	std::cout<<"argv[1]: "<<argv[1]<<std::endl;
+	if(argc > 1){
+		Max_Data_Size = std::stoi(argv[1]);
+	}
+
 	std::vector<int> datos;
 
-	const int Max_Data_Size = 20;
+	std::cout<<"construyendo datos"<<std::endl;
 	for(int i=0; i <= Max_Data_Size; i++){
 		datos.push_back(distribution(generator));
 	}
-
+	std::cout<<"construyendo datos"<<std::endl;
 	/*for(int i=1; i<(int)datos.size(); i++){
 		std::cout << "datos["<< i <<"] = ";
 		std::cout << datos.at(i-1) << std::endl;
 	}*/
 
-	std::cout << "MaxSubSum1 = " << maxSubSum1(datos) << std::endl;
-	std::cout << "MaxSubSum2 = " << maxSubSum2(datos) << std::endl;
-	std::cout << "MaxSubSum3 = " << maxSubSum3(datos) << std::endl;
-	std::cout << "MaxSubSum4 = " << maxSubSum3(datos) << std::endl;
-	std::cout << "datos.size() = " << datos.size()-1 << std::endl;
-	std::cout << "Max Data sise - 2 = " << Max_Data_Size << " is " << datos.at(datos.size()-2) << std::endl;
+	std::cout << "MaxSubSum1 = "<< std::endl;
+	std::cout <<"  "<< maxSubSum1(datos) << std::endl;
+	std::cout << "MaxSubSum2 = "<< std::endl;
+	std::cout <<"  " << maxSubSum2(datos) << std::endl;
+	std::cout << "MaxSubSum3 = "<< std::endl;
+	std::cout <<"  " << maxSubSum3(datos) << std::endl;
+	std::cout << "MaxSubSum4 = "<< std::endl;
+	std::cout <<"  " << maxSubSum4(datos) << std::endl;
+	//std::cout << "datos.size() = " << datos.size()-1 << std::endl;
+	//std::cout << "Max Data sise - 2 = " << Max_Data_Size << " is " << datos.at(datos.size()-2) << std::endl;
 	return 0;
 }
